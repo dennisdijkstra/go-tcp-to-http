8b94aac6cdfd9b58e450c312b59f98549a137ef4
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-02-20 20:58:51 +0100

    Read text file using buffer

--- main.go
+++ main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer close(lines)
+		defer f.Close()
+
+		buffer := make([]byte, 8)
+		currLine := ""
+
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Println(err)
+				break
+			}
+
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- currLine + parts[i]
+				currLine = ""
+			}
+			currLine += parts[len(parts)-1]
+		}
+		if currLine != "" {
+			lines <- currLine
+		}
+	}()
+	return lines
+}
 
 func main() {
 	f, err := os.Open("./messages.txt")
@@ -12,21 +48,11 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
 	defer f.Close()
 
-	buf := make([]byte, 8)
-	for {
-		n, err := f.Read(buf)
-		if err == io.EOF {
-			break
-		}
+	lines := getLinesChannel(f)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Printf("read: %s\n", string(buf[:n]))
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
