bc5e8406ded1f1c26422c8c0c4b872acc31ec741
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-03-04 18:17:35 +0100

    Parse from stream

--- internal/request/request.go
+++ internal/request/request.go
@@ -9,7 +9,16 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+
+	state requestState
 }
+
+type requestState int
+
+const (
+	requestStateInitialized requestState = iota
+	requestStateDone
+)
 
 type RequestLine struct {
 	HttpVersion   string
@@ -17,63 +26,109 @@ type RequestLine struct {
 	Method        string
 }
 
+const crlf = "\r\n"
+const bufferSize = 8
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	rawBytes, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+	buf := make([]byte, bufferSize)
+	readToIndex := 0
+	req := &Request{
+		state: requestStateInitialized,
 	}
 
-	requestLine, err := parseRequestLine(rawBytes)
-	if err != nil {
-		return nil, err
+	for req.state != requestStateDone {
+		if readToIndex >= len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf)
+			buf = newBuf
+		}
+
+		numBytesRead, err := reader.Read(buf[readToIndex:])
+		if err != nil {
+			if err == io.EOF {
+				req.state = requestStateDone
+				break
+			}
+			return nil, err
+		}
+		readToIndex += numBytesRead
+
+		numBytesParsed, err := req.parse(buf[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buf, buf[numBytesParsed:readToIndex])
+		readToIndex -= numBytesParsed
 	}
 
-	return &Request{
-		RequestLine: *requestLine,
-	}, nil
+	return req, nil
 }
 
-func parseRequestLine(data []byte) (*RequestLine, error) {
-	index := bytes.Index(data, []byte("\r\n"))
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.state {
+	case requestStateInitialized:
+		requestLine, bytesConsumed, err := parseRequestLine(data)
+
+		if err != nil {
+			return 0, err
+		}
+
+		if bytesConsumed > 0 {
+			r.RequestLine = *requestLine
+			r.state = requestStateDone
+		}
+
+		return bytesConsumed, nil
+	case requestStateDone:
+		return 0, errors.New("We're done already")
+	default:
+		return 0, errors.New("An unknown error occured")
+	}
+}
+
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
+	index := bytes.Index(data, []byte(crlf))
 	if index == -1 {
-		return nil, errors.New("Invalid request")
+		return nil, 0, nil
 	}
 
 	requestLineString := string(data[:index])
 
 	parts := strings.Split(requestLineString, " ")
 	if len(parts) != 3 {
-		return nil, errors.New("Invalid request")
+		return nil, 0, errors.New("Invalid request")
 	}
 
 	method := parts[0]
 	err := isAllUppercase(method)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 
 	requestTarget := parts[1]
 
 	versionParts := strings.Split(parts[2], "/")
 	if len(versionParts) != 2 {
-		return nil, errors.New("Invalid request")
+		return nil, 0, errors.New("Invalid request")
 	}
 
 	httpPart := versionParts[0]
 	if httpPart != "HTTP" {
-		return nil, errors.New("Invalid request")
+		return nil, 0, errors.New("Invalid request")
 	}
 
 	version := versionParts[1]
 	if version != "1.1" {
-		return nil, errors.New("Invalid request")
+		return nil, 0, errors.New("Invalid request")
 	}
 
+	noBytesConsumed := index + len(crlf)
 	return &RequestLine{
 		Method:        method,
 		RequestTarget: requestTarget,
 		HttpVersion:   version,
-	}, nil
+	}, noBytesConsumed, nil
 }
 
 func isAllUppercase(method string) error {
