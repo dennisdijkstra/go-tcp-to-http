d1819ccf203899544d9821d1b3da9d420b8dbe48
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-03-06 11:27:26 +0100

    Parse first header line

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,12 +1,47 @@
 package headers
 
+import (
+	"bytes"
+	"errors"
+	"strings"
+)
+
 type Headers map[string]string
+
+const crlf = "\r\n"
 
 func NewHeaders() Headers {
 	return make(Headers)
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	// Parse headers
-	return 0, false, nil
+	if h == nil {
+		return 0, false, errors.New("nil headers")
+	}
+
+	index := bytes.Index(data, []byte(crlf))
+	if index == -1 {
+		return 0, false, nil
+	}
+
+	if index == 0 {
+		return len(crlf), true, nil
+	}
+
+	headerLine := data[:index]
+
+	parts := strings.SplitN(string(headerLine), ":", 2)
+	if len(parts) != 2 {
+		return 0, false, errors.New("Invalid header format")
+	}
+
+	key := parts[0]
+	if key != strings.TrimSpace(key) {
+		return 0, false, errors.New("Invalid header format")
+	}
+
+	value := strings.TrimSpace(parts[1])
+	h[key] = value
+
+	return index + len(crlf), false, nil
 }
