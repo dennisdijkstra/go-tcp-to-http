ca3720c9c77c235023d6bd950a718e4abddd9818
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-03-10 20:15:23 +0100

    Init response package

---
+++ internal/response/response.go
@@ -0,0 +1,20 @@
+package response
+
+import (
+	"fmt"
+	"io"
+)
+
+type StatusCode int
+
+const (
+	StatusOK                  StatusCode = 200
+	StatusBadRequest          StatusCode = 400
+	StatusInternalServerError StatusCode = 500
+)
+
+func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+	fmt.Println("Writing status line", statusCode)
+
+	return nil
+}
