a658816191265432b18b425398b781ffc5bbc846
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-03-10 20:12:30 +0100

    Build server integration

---
+++ internal/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"log"
+	"net"
+	"strconv"
+)
+
+type Server struct {
+	listener net.Listener
+}
+
+func (s *Server) Close() error {
+	return s.listener.Close()
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!"))
+}
+
+func Serve(port int) (*Server, error) {
+	l, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Server{listener: l}
+	go s.listen()
+
+	return s, nil
+}
