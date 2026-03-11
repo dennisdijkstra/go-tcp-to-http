9c689ce4f834185e6d2282977a6c1bff8c7c53c1
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-03-11 16:16:23 +0100

    Move statusline logic to own file

--- internal/response/response.go
+++ internal/response/response.go
@@ -8,36 +8,6 @@ import (
 	"github.com/dennisdijkstra/go-tcp-to-http/internal/headers"
 )
 
-type StatusCode int
-
-const (
-	StatusOK                  StatusCode = 200
-	StatusBadRequest          StatusCode = 400
-	StatusInternalServerError StatusCode = 500
-)
-
-func getStatusLine(statusCode StatusCode) []byte {
-	reasonPhrase := ""
-
-	switch statusCode {
-	case StatusOK:
-		reasonPhrase = "OK"
-	case StatusBadRequest:
-		reasonPhrase = "Bad Request"
-	case StatusInternalServerError:
-		reasonPhrase = "Internal Server Error"
-	default:
-		reasonPhrase = "Unknown"
-	}
-
-	return []byte(fmt.Sprintf("HTTP/1.1 %d %s\r\n", statusCode, reasonPhrase))
-}
-
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
-	_, err := w.Write(getStatusLine(statusCode))
-	return err
-}
-
 func GetDefaultHeaders(contentLen int) headers.Headers {
 	headers := headers.NewHeaders()
 
